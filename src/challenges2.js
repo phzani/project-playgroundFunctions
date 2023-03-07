cd6ce3a617592c5f5ee2a42e4ae3cc5d76629ccc
Pedro Zani <ph.zani@icloud.com>
2023-03-06 23:11:08 -0300

    projeto finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,22 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+const generatePhoneNumber = (numero) => {
+  if (numero.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
 
+  const numInvalido = (num, index) => num < 0 || num > 9
+  || (numero.indexOf(num) !== index && numero.filter((n) => n === num).length >= 3);
+
+  if (numero.some(numInvalido)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let ddd = numero.slice(0, 2).join('');
+  let numero1 = numero.slice(2, 7).join('');
+  let numero2 = numero.slice(7).join('');
+
+  return `(${ddd}) ${numero1}-${numero2}`;
+};
 // Desafio 12 -  Crie a função triangleCheck
 const triangleCheck = (lineA, lineB, lineC) => {
   let checkLine = Math.abs(lineB - lineC);
