c2e4c46ee56530ec84dc948f22c4b02f26f535f6
Pedro Zani <ph.zani@icloud.com>
2023-03-06 22:18:51 -0300

    adicionando exercicios bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,30 @@
 // Desafio 11 - Crie a função generatePhoneNumber
 
 // Desafio 12 -  Crie a função triangleCheck
+const triangleCheck = (lineA, lineB, lineC) => {
+  let checkLine = Math.abs(lineB - lineC);
+  if (lineA < (lineB + lineC) && lineA > checkLine) {
+    return true;
+  }
+  return false;
+};
 
 // Desafio 13 - Crie a função hydrate
-
+const hydrate = (string) => {
+  let separaNumeros = /\d+/g;
+  let bebidas = string.match(separaNumeros);
+  let soma = 0;
+  for (let index = 0; index < bebidas.length; index += 1) {
+    soma += parseInt(bebidas[index], 0);
+  }
+  let coposDeAgua;
+  if (soma === 1) {
+    coposDeAgua = '1 copo de água';
+  } else {
+    coposDeAgua = `${soma} copos de água`;
+  }
+  return coposDeAgua;
+};
 /* eslint no-undef: 0 */
 
 // Não modifique essas linhas
